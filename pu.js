f01570aa702dab78fd356a9b17c94bdab796ed30
Randommss <94282914+Randommss@users.noreply.github.com>
2021-11-14 13:42:49 +0545

    random commit

---
+++ pu.js
@@ -0,0 +1,13 @@
+var p=document.getElementById("K");
+function Management()
+if(p.textContent.includes('Management'))
+{
+{
+    var p=document.getElementById("Mangement");
+    p.style.color="green";
+}
+else
+{
+    p.style.color="red";
+}
+}
