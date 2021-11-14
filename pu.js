155108bf1d1bc8c6933aa028c05e6c8a4a945d50
Randommss <94282914+Randommss@users.noreply.github.com>
2021-11-14 15:09:57 +0545

    Add files via upload

--- pu.js
+++ pu.js
@@ -1,4 +1,5 @@
-function Management()
+
+    function Management()
 {
     var write=document.getElementById("Management");
     write.style.color="limegreen";
@@ -9,16 +10,39 @@ function Management()
     var write3=document.getElementById("Humanities");
     write3.style.visibility="hidden";
 }
-function Science()
+    function Science()
 {
-    var write1=document.getElementById("Science");
-    write1.style.color="red";
-    var write=document.getElementById("Management");
-    write.style.visibility="hidden";
-    var write2=document.getElementById("Health");
-    write2.style.visibility="hidden";
-    var write3=document.getElementById("Humanities");
-    write3.style.visibility="hidden";
+    var write4=document.getElementById("Management");
+    write4.style.visibility="hidden";
+    var write5=document.getElementById("Science");
+    write5.style.color="limegreen";
+    var write6=document.getElementById("Health");
+    write6.style.visibility="hidden";
+    var write7=document.getElementById("Humanities");
+    write7.style.visibility="hidden";
 }
+    function Health()
+{
+    var write8=document.getElementById("Management");
+    write8.style.visibility="hidden";
+    var write9=document.getElementById("Science");
+    write9.style.visibility="hidden";
+    var write10=document.getElementById("Health");
+    write10.style.color="limegreen";
+    var write11=document.getElementById("Humanities");
+    write11.style.visibility="hidden";
+}
+    function Humanities()
+{
+    var write12=document.getElementById("Management");
+    write12.style.visibility="hidden";
+    var write13=document.getElementById("Science");
+    write13.style.visibility="hidden";
+    var write14=document.getElementById("Health");
+    write14.style.visibility="hidden";
+    var write15=document.getElementById("Humanities");
+    write15.style.color="limegreen";
+}
+
 
 
