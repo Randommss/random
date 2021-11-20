650f1f1b22cb7ea2e9941ad48cf7645645533b7f
DESKTOP-BJ8IQQT\Dell <kritika.pantha61@gmail.com>
2021-11-20 21:49:46 +0545

    finally

--- pu.js
+++ pu.js
@@ -43,6 +43,49 @@
     var write15=document.getElementById("Humanities");
     write15.style.visibility="visible";
 }
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+var i=0;
+function read()
+{
+    if(!i)
+    {
+        document.getElementById("more").style.
+        display="inline";
+        document.getElementById("dots").style.
+        display="none";
+        document.getElementById("read").
+        innerHTML="Read less";
+        i=1;
+    }
+    else
+    {
+        document.getElementById("more").style.
+        display="none";
+        document.getElementById("dots").style.
+        display="inline";
+        document.getElementById("read").
+        innerHTML="Read more";
+        i=0; 
+    }
+}
 
 
 
